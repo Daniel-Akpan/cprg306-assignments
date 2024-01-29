9da95939b2d75504612ae710aa8d96a4cb5e48dd
Daniel-Akpan <95925992+TW-Warlord@users.noreply.github.com>
2024-01-29 13:52:21 -0700

    testing
    testing

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Link to week-2 page
-      </Link>
+      <Link href="/week-2">Link to week-2 page </Link>
+      <h1></h1>
     </main>
   );
 }
