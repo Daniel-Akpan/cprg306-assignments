ceaef8515090c2fd5ead662974ac7576a95c5e94
Daniel-Akpan <95925992+TW-Warlord@users.noreply.github.com>
2024-01-29 14:05:11 -0700

    adding week 3

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
 
-export default function Home() {
+export default function page() {
   return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <Link href="/week-2">Link to week-2 page </Link>
-      <h1></h1>
+    <main className="max-w-2xl mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">CPRG 306: Web Development 2 - Assignments</h1>
+      <Link className="text-blue-500 hover:underline " href="/week-2">Link to week-2 page</Link>
+      <p><Link className="text-blue-500 hover:underline " href="/week-3">Link to week-3 page</Link></p>
     </main>
   );
 }
